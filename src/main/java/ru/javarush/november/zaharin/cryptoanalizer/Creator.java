0510fec931a211b26c1a4d4c85a02baab70baf19
Misha Zaharin <zakharin.mikhail@gmail.com>
2022-09-12 14:58:39 +0300

    1. made coding and decoding. 2. thing about characters that are not in our cryptographic alphabet - just skip them.

--- src/main/java/ru/javarush/november/zaharin/cryptoanalizer/Creator.java
+++ src/main/java/ru/javarush/november/zaharin/cryptoanalizer/Creator.java
@@ -21,26 +21,17 @@ public class Creator {
         ///....
         return Integer.parseInt(null);
     }
-
     public File coding(String path, int step) {
+        // think about "n\"
         File file2 = new File("coding_text.txt");
         try (FileReader fileReader = new FileReader(path);
              FileWriter fileWriter = new FileWriter(file2);
              BufferedReader bufferedReader = new BufferedReader(fileReader, alphabet.length)) {
             int counter;
             while ((counter = bufferedReader.read()) != -1) {
-                // work with (char)counter
-                for (int i = 0; i < alphabet.length; i++) {
-                    // compare (char)counter
-                    char result = (char) counter;
-                    if (alphabet[i] == result) {
-                        result = alphabet[(i + step) % alphabet.length];
-                        fileWriter.write(result);
-                    } else {
-                        result = (char) counter;
-                        fileWriter.write(result);
-                    }
-                }
+                char result = (char) counter;
+                result = findNewSymbolCoding(step, result);
+                fileWriter.write(result);
             }
             fileWriter.flush();
         } catch (FileNotFoundException e) {
@@ -52,44 +43,15 @@ public class Creator {
     }
 
     public File deCoding(String path, int step) {
+        // think about "n\"
         File file2 = new File("deCoding_text.txt");
-        try (FileReader fileReader = new FileReader(path);
+        try (FileReader fileReader = new FileReader("coding_text.txt");
              FileWriter fileWriter = new FileWriter(file2);
              BufferedReader bufferedReader = new BufferedReader(fileReader, alphabet.length)) {
             int counter;
             while ((counter = bufferedReader.read()) != -1) {
-                // work with (char)counter
-                for (int i = 0; i < alphabet.length; i++) {
-                    // compare (char)counter
-                    char result = (char) counter;
-                    if (alphabet[i] == result) {
-                        result = alphabet[(i - step) % alphabet.length];
-                        fileWriter.write(result);
-                    } else {
-                        result = (char) counter;
-                        fileWriter.write(result);
-                    }
-                }
-            }
-            fileWriter.flush();
-        } catch (FileNotFoundException e) {
-            System.out.println("there is no file");
-        } catch (IOException e) {
-            e.getStackTrace();
-        }
-        return file2;
-    }
-
-    public File easyCoding(String path, int step) {
-        File file2 = new File("easyCoding_text.txt");
-        try (FileReader fileReader = new FileReader(path);
-             FileWriter fileWriter = new FileWriter(file2);
-             BufferedReader bufferedReader = new BufferedReader(fileReader, alphabet.length)) {
-            int counter;
-            char result = ' ';
-            while ((counter = bufferedReader.read()) != -1) {
-                // work with (char)counter
-                result = (char) (counter + step);
+                char result = (char) counter;
+                result = findNewSymbolDeCoding(step, result);
                 fileWriter.write(result);
             }
             fileWriter.flush();
@@ -101,24 +63,38 @@ public class Creator {
         return file2;
     }
 
-    public File easyDeCoding(String path, int step) {
-        File file2 = new File("easyDeCoding_text.txt");
-        try (FileReader fileReader = new FileReader(path);
-             FileWriter fileWriter = new FileWriter(file2);
-             BufferedReader bufferedReader = new BufferedReader(fileReader, alphabet.length)) {
-            int counter;
-            char result = ' ';
-            while ((counter = bufferedReader.read()) != -1) {
-                // work with (char)counter
-                result = (char) (counter - step);
-                fileWriter.write(result);
-            }
-            fileWriter.flush();
-        } catch (FileNotFoundException e) {
-            System.out.println("there is no file");
-        } catch (IOException e) {
-            e.getStackTrace();
+    public char findNewSymbolCoding(int step, char result) { // Andrii Shylin (UA Kyiv)
+//        findCurrentPosition returns -1 if current symbol is not present in alphabet
+        int currentPosition = findCurrentPosition(result);
+        if (currentPosition == -1) {
+            return result;
         }
-        return file2;
+        int normalizedDelta = Math.abs(step % alphabet.length);
+        int newIndex = (currentPosition + normalizedDelta) % alphabet.length;
+        return alphabet[newIndex];
+    }
+
+    public char findNewSymbolDeCoding(int step, char result) { // Andrii Shylin (UA Kyiv)
+//        findCurrentPosition returns -1 if current symbol is not present in alphabet
+        int currentPosition = findCurrentPosition(result);
+        if (currentPosition == -1) {
+            return result;
+        }
+        int normalizedDelta = Math.abs(step % alphabet.length);
+        int newIndex = (currentPosition - normalizedDelta) % alphabet.length;
+        if (newIndex < 0) {
+            newIndex = alphabet.length - Math.abs(newIndex);
+        }
+        return alphabet[newIndex];
+    }
+
+    private int findCurrentPosition(char result) {
+        int findCurrentPosition = 0;
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == result) {
+                findCurrentPosition = i;
+            }
+        }
+        return findCurrentPosition;
     }
 }
